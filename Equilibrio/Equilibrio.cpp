2c144e963e0c59f9496b9bdac6de5e8f1ed5352e
Alvaro <montero.barraza.alvaro.david@gmail.com>
2022-04-13 18:25:15 -0500

    equilibrio cojn biblioteca stl

--- Equilibrio/Equilibrio.cpp
+++ Equilibrio/Equilibrio.cpp
@@ -5,14 +5,53 @@
 =======================================
  */
 #include<iostream>
+#include<string>
+#include<stack>
 #include<stdlib.h>
 #include<equilibrio.h>
 using namespace std;
 
 int main(){
+    stack<char> signo;
     string cadena;
+    bool flag;
+    int tam;
     cout<<"Introduce un string para ser analizado";
     cin>>cadena;
+    tam=cadena.size();
+    for (int i = 0; i < tam; i++)
+    {
+        if (cadena[i]=='(')
+        {
+            signo.push(cadena[i]);
+        }else if(cadena[i]=='[' ){
+                signo.push(cadena[i]);
+        }else if(cadena[i]==')'){
+            if (signo.top()==cadena[i])
+            {
+                flag=true;
+                signo.pop();
+            }else{
+                flag=false;
+            }
+        
+        }else if(cadena[i]==']'){
+            if(signo.top()==cadena[i]){
+                flag=true;
+                signo.pop();
+            }else{
+                flag=false;
+            }
+        }
+        
+    }
+    if(flag){
+        cout<<"Existe equilibrio";
+    }else{
+        cout<<"No existe equilibrio";
+    }
+    system("pause");
+    
 
 
 
