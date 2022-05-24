07bbb208c4f23438d7594ecc4c85c0e63b2e31c4
Alvaro <montero.barraza.alvaro.david@gmail.com>
2022-05-24 00:04:42 -0500

    nuevo nodo subrutine

--- Arboles/lib.h
+++ Arboles/lib.h
@@ -1,4 +1,5 @@
-#include"iostream"
+#include<iostream>
+using namespace std;
 //================================================================
 typedef struct{
     int numero;
@@ -11,19 +12,54 @@ typedef struct Nodo{
 
 }Nodo;
 //================================================================
-void C_nuevoNodo(Nodo **Hoja,int numero){
+void A_nuevoNodo(Nodo **Hoja,int numero){
     if (Hoja==NULL)
     {
-        *Hoja=new Nodo();
+        Nodo *nuevo=C_nuevoNodo(numero);
+        return;
     }
+    if ((*Hoja)->clave==numero)
+    {
+        cout<<"El valor ya existe";
+        return;
+    }
+    if ((*Hoja)->clave>numero)//izquierda
+    {
+        A_nuevoNodo((Hoja)->izquierda,numero);
+    }
+    
+    
     
 
 }
 
 void menu(){
     int opt;
-    cout<<"1.-Crear Nodo"<<endl<<"2.-Eliminar nodos"<<endl;
-    cin<<opt;
-    
+    cout<<"1.-Crear Nodo"<<endl<<"2.-Eliminar nodos"<<endl<<"3.-Imprimir nodos en preorden"<<endl<<"4.-Imprimir en postorden";
+    cin>>opt;
+    switch (opt)
+    {
+    case 1:
+        
+        break;
+    case 2:
+        break;
+    case 3:
+        break;
+    case 4:
+        break;
+    cout<<"Introduce una opcion valida"<<endl;
+    system("pause");
+    system("cls");
+        break;
+    }
 
+
+}
+
+Nodo* C_nuevoNodo(int valor){
+    Nodo *a=new Nodo();
+    a->clave=valor;
+    a->derecha=a->izquierda=nullptr;
+    return a;
 }
