d5f6194a040a87eb23a4d9b9aa2fbd228deada67
AlvaroMonteroB <montero.barraza.alvaro.david@gmail.com>
2022-05-25 17:15:06 -0500

    add prototipos de funciones y preorden

--- Arboles/lib.h
+++ Arboles/lib.h
@@ -4,6 +4,7 @@ using namespace std;
 typedef struct{
     int numero;
 }Valor;
+
 typedef struct Nodo{
         int clave;
         Valor cant;
@@ -12,20 +13,32 @@ typedef struct Nodo{
 
 }Nodo;
 //================================================================
-void A_nuevoNodo(Nodo **Hoja,int numero){
+void menu();
+Nodo* C_nuevoNodo(int valor);
+void A_nuevoNodo(Nodo *&Hoja,int numero);
+void Preorden(Nodo *R);
+
+//================================================================
+void A_nuevoNodo(Nodo *&Hoja,int numero){//Paso por referencia
     if (Hoja==NULL)
     {
-        Nodo *nuevo=C_nuevoNodo(numero);
+        Nodo *Hoja=C_nuevoNodo(numero);
         return;
     }
-    if ((*Hoja)->clave==numero)
+    if ((Hoja)->clave==numero)
     {
         cout<<"El valor ya existe";
         return;
     }
-    if ((*Hoja)->clave>numero)//izquierda
+    if ((Hoja)->clave>numero)//izquierda
     {
         A_nuevoNodo((Hoja)->izquierda,numero);
+        return;
+    }
+    if (Hoja->clave<numero)
+    {
+        A_nuevoNodo(Hoja->derecha,numero);
+        return;
     }
     
     
@@ -34,17 +47,28 @@ void A_nuevoNodo(Nodo **Hoja,int numero){
 }
 
 void menu(){
-    int opt;
+    int opt,num;
+    Nodo *raiz;
     cout<<"1.-Crear Nodo"<<endl<<"2.-Eliminar nodos"<<endl<<"3.-Imprimir nodos en preorden"<<endl<<"4.-Imprimir en postorden";
     cin>>opt;
     switch (opt)
     {
     case 1:
-        
+        cout<<"Introduce una clave"<<endl;
+        cin>>num;
+        A_nuevoNodo(raiz,num);
         break;
     case 2:
         break;
     case 3:
+    if (raiz)
+    {
+        Preorden(raiz);
+    }else{
+        cout<<"No hay arbol"<<endl;
+    }
+    
+    
         break;
     case 4:
         break;
@@ -62,4 +86,12 @@ Nodo* C_nuevoNodo(int valor){
     a->clave=valor;
     a->derecha=a->izquierda=nullptr;
     return a;
+}
+
+void Preorden(Nodo *R){
+    if(R){
+        cout<<R->clave;
+        Preorden(R->izquierda);
+        Preorden(R->derecha);
+    }
 }
